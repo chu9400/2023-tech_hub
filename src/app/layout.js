28433efa0c0efdabdc23bf07c0359c512773e210
Hanul <chu9400@naver.com>
2023-10-12 17:16:09 +0900

    탭 이미지 변경

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,13 +2,14 @@ import "@/assets/scss/style.scss";
 
 export const metadata = {
   title: "Tech Hub",
-  description: "나만의 학습 사이트를 컨셉으로 만든 페이지입니다.",
-  keywords: ["포트폴리오, 공부, 학습, 넥스트, next.js"],
+  description: "TechHub 라는 학습 사이트를 컨셉으로 만든 페이지입니다.",
+  keywords: ["TechHub 포트폴리오, 공부, 학습, 넥스트, next.js"],
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
+      <link rel="icon" type="image/svg+xml" href="favicon.svg" />
       <body>{children}</body>
     </html>
   )
